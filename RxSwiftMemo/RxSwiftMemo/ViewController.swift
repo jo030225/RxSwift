1320ec2a2fb272ee89a06ef056d0ea9a007f75ad
jo030225 <whwngur123@naver.com>
2021-07-15 21:07:24 +0900

    [CREATE] MVVM 메모장 프로젝트 생성

---
+++ RxSwiftMemo/RxSwiftMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftMemo
+//
+//  Created by 조주혁 on 2021/07/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
